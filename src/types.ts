69596601689d90f8bffd409cf5daad5311c05ec5
Ashutosh Bansal <ashutosh.bansal@sourcefuse.com>
2021-07-13 18:29:56 +0530

    Dependency removed from aws-sdk

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,14 @@
+import {S3} from '@aws-sdk/client-s3';
 import {BindingKey} from '@loopback/core';
 
 export namespace AWSS3Bindings {
-  export const AwsS3Provider = BindingKey.create<AWS.S3>('sf.aws.s3');
+  export const AwsS3Provider = BindingKey.create<S3>('sf.aws.s3');
   export const Config = BindingKey.create<AwsS3Config>('sf.aws.s3.config');
 }
 
 export interface AwsS3Config {
   accessKeyId: string;
   secretAccessKey: string;
+  region: string;
   signatureVersion: string;
 }
