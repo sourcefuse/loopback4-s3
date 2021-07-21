0cf2c4aa06583814fbfb97ce46f4ac7f74f54c3c
ashutosh-bansal-2136 <82804130+ashutosh-bansal-2136@users.noreply.github.com>
2021-07-21 16:53:21 +0530

    Revert "fix(loopback4-s3): dependency removed from aws-sdk #13"

--- src/providers/aws-s3.provider.ts
+++ src/providers/aws-s3.provider.ts
@@ -1,18 +1,15 @@
-//eslint-disable @typescript-eslint/no-explicit-any
 import {Provider, inject} from '@loopback/core';
-import * as AWS from '@aws-sdk/client-s3';
+import * as AWS from 'aws-sdk';
 import {AwsS3Config, AWSS3Bindings} from '../types';
 
 export class AwsS3Provider implements Provider<AWS.S3> {
-  //eslint-disable-next-line @typescript-eslint/no-explicit-any
-  client: any;
   constructor(
     @inject(AWSS3Bindings.Config)
     private readonly config: AwsS3Config,
   ) {
-    this.client = new AWS.S3Client(this.config);
+    AWS.config.update(Object.assign({}, {signatureVersion: this.config.signatureVersion}, this.config));
   }
   value() {
-    return new AWS.S3(this.client);
+    return new AWS.S3();
   }
 }
