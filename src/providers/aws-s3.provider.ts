c2c9c5b640cba9fe7b802c3020d59b47a0c60726
Ashutosh Bansal <ashutosh.bansal@sourcefuse.com>
2021-09-02 15:37:57 +0530

    feat(component): dependency removed on entire package
    gh-20

--- src/providers/aws-s3.provider.ts
+++ src/providers/aws-s3.provider.ts
@@ -1,21 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {Provider, inject} from '@loopback/core';
-import * as AWS from 'aws-sdk';
+import * as AWS from '@aws-sdk/client-s3';
 import {AwsS3Config, AWSS3Bindings} from '../types';
 
 export class AwsS3Provider implements Provider<AWS.S3> {
+  client: any;
   constructor(
     @inject(AWSS3Bindings.Config)
     private readonly config: AwsS3Config,
   ) {
-    AWS.config.update(
-      Object.assign(
-        {},
-        {signatureVersion: this.config.signatureVersion ?? 'v4'},
-        this.config,
-      ),
-    );
+    this.client = new AWS.S3Client(this.config);
   }
   value() {
-    return new AWS.S3();
+    return new AWS.S3(this.client);
   }
 }
