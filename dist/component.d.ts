49fbe8c81cd16e55a7734c44aae24ed86a1e8904
Shubham P <shubham.prajapat@sourcefuse.com>
2023-07-11 17:12:35 +0530

    refactor(chore): remove dist
    it's a build folder that doesn't need to be tracked
    
    GH-138

--- dist/component.d.ts
+++
@@ -1,5 +0,0 @@
-import { Component, ProviderMap } from '@loopback/core';
-export declare class AwsS3Component implements Component {
-    constructor();
-    providers?: ProviderMap;
-}
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './component';
-export * from './types';
-export * from './providers';
