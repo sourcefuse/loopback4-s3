84628db10b31f096ba14cb9c07d5d3646fdeb174
sahil-jassal-sf <sahil.jassal@sourcefuse.com>
2024-01-18 13:00:12 +0530

    chore(deps): update deps and devDeps version
    update @loopback and its deps, @aws-sdk package versions and
    replace depreacated ones for better linting and compatibility
    with lb4 cli
    
    GH-172

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 import {S3, ServiceInputTypes} from '@aws-sdk/client-s3';
 import {getSignedUrl} from '@aws-sdk/s3-request-presigner';
-import {Command} from '@aws-sdk/smithy-client';
+import {Command} from '@smithy/smithy-client';
 import {MetadataBearer, RequestPresigningArguments} from '@aws-sdk/types';
 import {BindingKey} from '@loopback/core';
 
